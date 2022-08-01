0330f594db8f8d2cd8696548c06cf5dbe84b9f04
Miloš Marković <73335561+milosmarkovic1701@users.noreply.github.com>
2022-08-01 07:38:09 +0200

    Initialize project

---
+++ bus_station/src/test/java/com/thesis/bus_station/BusStationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thesis.bus_station;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusStationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
